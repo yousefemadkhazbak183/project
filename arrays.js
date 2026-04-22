6845775806ebc45262b08aa26174230932e87b47
Yousef Emad <yousefemadkhazbak@gmail.com>
2026-04-22 16:34:27 +0200

    Define Arrays in JavaScript.

---
+++ arrays.js
@@ -0,0 +1,10 @@
+let productColors = ['Green', 'Red', 'Blue'];
+console.log(productColors[0]);
+
+productColors[0] = "Grey";
+console.log(productColors[0]);
+
+productColors[1] = 50;
+console.log(productColors[1]);
+
+console.log(productColors.length);
