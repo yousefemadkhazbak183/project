89054e31de899a17ac73ad5d6ef8be6c3bf3dd59
Yousef Emad <yousefemadkhazbak@gmail.com>
2026-04-22 16:47:28 +0200

    Define Functions in JavaScript.

---
+++ functions.js
@@ -0,0 +1,36 @@
+function sayHi(){
+    console.log("Hi");
+
+}
+
+sayHi();
+
+function sayHello(name){
+    console.log("Hello " + name);
+}
+
+sayHello("Yousef");
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+console.log(multiply(5, 10));
+
+function multiply(num1, num2){
+    const result = num1 * num2;
+    return result;
+}
+console.log(multiply(55, 13));
+
+function add(num1, num2){
+    const result = num1 + num2;
+    return result;
+
+}
+console.log(add(5, 10));
+
+function name(name1, name2){
+    const result = name1 + name2;
+    return result;
+}
+console.log(name('Yousef ', 'Emad'));
