6f7ca9e64341d5ffa475d47aaa5dbd972b9bc569
Yousef Emad <yousefemadkhazbak@gmail.com>
2026-04-22 16:25:41 +0200

    Define Variables.js primtive data type. dynamic object

---
+++ variables.js
@@ -0,0 +1,39 @@
+let firstName = 'Yousef';
+let lastName = 'Emad';
+
+console.log("firstName: "+ firstName , "lastName: "+ lastName);
+
+
+const myBirthDay = "18-03-2004";
+
+console.log("myBirthDay: "+ myBirthDay);
+
+let favoriteColor = "blue";
+
+let numberOfSiblings = 3;
+
+let isMarried = false;
+
+let largeNumber = 12345678901234567890n;
+
+let uniqueId = Symbol();
+
+let favoriteFood;
+console.log("favoriteFood: "+ favoriteFood);    
+
+let course = {
+    name: "JavaScript",
+    hours: 3
+}
+console.log(course['hours']);
+course['name'] = "Python";
+console.log(course['name']);
+
+let amount = 100000;
+console.log(typeof amount);
+
+amount = true;
+console.log(typeof amount);
+
+let property = 'hours';
+console.log(course[property]);
