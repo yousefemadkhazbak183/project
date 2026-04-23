9c61040c2d3e1eb11de1811bb194be331d42b26d
Yousef Emad <yousefemadkhazbak@gmail.com>
2026-04-23 16:07:26 +0200

    Define Comparsion Operators.

---
+++ control-flow.js
@@ -0,0 +1,13 @@
+    let num1 = 10;
+    let num2 = 4;
+
+    const sum = num1 > num2;
+    console.log(sum);
+
+    let num3 = 1;
+    let num4 = 4;
+
+    const sum2 = num3 <= num4;
+    console.log(sum);
+
+    
